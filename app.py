49b8bad5d0952b2f1e6749e647bcef96450a4565
Suryrajsinh Jadeja <suryrajsinhjadeja95@gmail.com>
2024-07-31 14:25:18 +0530

    Add new route

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, Kubernetes!'
 
+@app.route('/newroute')
+def new_route():
+    return 'This is a new route!'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
